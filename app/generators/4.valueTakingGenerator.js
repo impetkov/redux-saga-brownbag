ee9ec8adbfd9a68949fbcf24c44cfa7b9ca9c267
Ivan Petkov <ipetkov@appliedsystems.com>
2018-12-06 14:25:08 +0000

    Updated some comments

--- app/generators/4.valueTakingGenerator.js
+++ app/generators/4.valueTakingGenerator.js
@@ -11,8 +11,8 @@ const gen = generator();
 // calling the .next() method with an argument will resume the generator function execution, 
 // replacing the yield expression, where execution was paused, with the argument from next()
 
-console.log(gen.next());        // { value: 1, done: false }
-console.log(gen.next('a'));   // { value: 2, done: false }
-console.log(gen.next('b'));   // { value: 3, done: false }
-console.log(gen.next('3'));     // Final values of variables: one, two, 3
+console.log(gen.next());        // { value: a, done: false }
+console.log(gen.next('a'));     // { value: b, done: false }
+console.log(gen.next('b'));     // { value: 3, done: false }
+console.log(gen.next('3'));     // Final values of variables: a, b, 3
                                 // { value: undefined, done: true }
